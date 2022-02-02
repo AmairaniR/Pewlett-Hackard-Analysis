3c761202a6393447d9231a1cbfc6cd048ceba066
Amairani Rosario <amairanirosario@MacBook-Pro-2.fios-router.home>
2022-02-02 12:44:19 -0500

    added queries.sql and retirement_info csv

--- schema.sql
+++ schema.sql
@@ -48,3 +48,29 @@ CREATE TABLE titles (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
+DROP TABLE titles CASCADE;
+CREATE TABLE titles (
+	emp_no INT,
+	title VARCHAR NOT NULL, 
+	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+DROP TABLE titles CASCADE;
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL, 
+	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+DROP TABLE dept_emp CASCADE;
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL, 
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
